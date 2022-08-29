16441e275f83e15f5b8cbe5c3eb9e9fda5dc135a
akshatV21 <a.vishwakarma2110@gmail.com>
2022-08-29 16:49:25 +0530

    changed getRandomPokemon func

--- src/helpers/pokemon-helpers.js
+++ src/helpers/pokemon-helpers.js
@@ -1,12 +1,24 @@
-const config = require("../config")
 const PokeModel = require("../models/poke-model")
 
 const getRandomPokemon = async () => {
   try {
-    const totalPokemons = 6
-    const ramdomPokedexNumber = Math.ceil(Math.random() * totalPokemons)
+    const chance = Math.random()
+    let stage
+    if (chance < 0.1) {
+      stage = 3
+    } else if (chance < 0.4) {
+      stage = 2
+    } else {
+      stage = 1
+    }
 
-    const randomPokemon = await PokeModel.findOne({ pokedexNo: ramdomPokedexNumber })
+    const randomPokemons = await PokeModel.find({ stage: stage })
+
+    const pokedexNos = randomPokemons.map(poke => poke.pokedexNo)
+    const totalPokemons = randomPokemons.length
+    const ramdomPokedexNumber = pokedexNos[Math.ceil(Math.random() * totalPokemons)]
+    const randomPokemon = randomPokemons[ramdomPokedexNumber]
+
     return { ...randomPokemon._doc }
   } catch (error) {
     console.log(error)
